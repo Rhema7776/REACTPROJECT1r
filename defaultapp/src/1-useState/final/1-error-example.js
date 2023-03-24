a339ba5208cc7404884289bda2e2cd733528ae8f
Rhema7776 <rhemachukwumah7@gmail.com>
2023-03-24 06:06:16 +0100

    first commit

--- defaultapp/src/1-useState/final/1-error-example.js
+++ defaultapp/src/1-useState/final/1-error-example.js
@@ -10,10 +10,11 @@ const ErrorExample = () => {
   return(
     <React.Fragment>
         <h2>{title}</h2>
-        <button type='button' className='btn' 
+        <button type='button' className='btn btn-primary' 
         onClick={handleClick}>
             change title
         </button>
+        <p>Check your console to see what happens</p>
     </React.Fragment>
   );
 }
